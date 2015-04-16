60be10096929f45607fbfd0a4043a692f44a5e86
Roberto Palamaro <roby.palamaro@gmail.com>
2015-04-16 17:26:16 +0200

    Done all predicates and rules

--- sinalgo-0.75.3/src/projects/matchingSample/nodes/nodeImplementations/MSNode.java
+++ sinalgo-0.75.3/src/projects/matchingSample/nodes/nodeImplementations/MSNode.java
@@ -12,18 +12,105 @@ import sinalgo.nodes.Connections;
 import sinalgo.nodes.Node;
 import sinalgo.nodes.edges.Edge;
 import sinalgo.nodes.messages.Inbox;
+import sinalgo.tools.Tools;
 import sinalgo.tools.logging.Logging;
+//			t.startRelative(Tools.getRandomNumberGenerator().nextDouble(), this);
 
 public class MSNode extends Node {
 
 	public static boolean isSending = true;
 	boolean isMarried;
 	Integer pointingNode; 
+	boolean isEligibile;
 	int interval;
 	
 	Logging myLog = Logging.getLogger("myLog.txt");
 
+	private int checkNeighborForMarriage(){
+		Connections conn = this.outgoingConnections;
+		Iterator<Edge> it = conn.iterator();
+		while(it.hasNext()){
+			MSNode temp = (MSNode) it.next().endNode;
+			if(temp.pointingNode == this.ID){
+				return temp.ID;
+			}
+		}
+		return -1;
+	}
 	
+	private int checkMaxNeighborForMarriageWithNullPreference(){
+		Connections conn = this.outgoingConnections;
+		Iterator<Edge> it = conn.iterator();
+		Integer t = -1;
+		while(it.hasNext()){
+			MSNode temp = (MSNode)it.next().endNode;
+			if(temp.pointingNode == -1 && temp.ID > this.ID && temp.isMarried == false){
+				if(temp.ID > t){
+					t = temp.ID;
+				}
+			}
+		}
+		return t;
+	}
+	
+	private MSNode getNodeByID(int id){
+		Connections conn = this.outgoingConnections;
+		Iterator<Edge> it = conn.iterator();
+		while(it.hasNext()){
+			MSNode temp = (MSNode)it.next().endNode;
+			if(temp.ID == id){
+				return temp;
+			}
+		}
+		return null;
+	}
+	boolean PRmarried(){
+		Connections conn = this.outgoingConnections;
+		Iterator<Edge> it = conn.iterator();
+		while(it.hasNext()){
+			MSNode temp = (MSNode) it.next().endNode;
+			if(temp.pointingNode == this.ID){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	
+	
+	boolean updateRules(){
+		boolean flag = this.PRmarried();
+		if(this.isMarried != flag){
+			this.isMarried = flag;
+			return true;
+		}
+		return false;
+	}
+	
+	boolean marriageRule(){
+		int j = -1;
+		if((this.isMarried == this.PRmarried()) && this.pointingNode == -1 && (j=this.checkNeighborForMarriage())!=-1){
+			this.pointingNode = j;
+			return true;
+		}
+		return false;
+	}
+	
+	void seductionRule(){
+		int j;
+		if((this.isMarried == this.PRmarried()) && this.pointingNode == -1 && this.checkNeighborForMarriage()==-1 && (j = this.checkMaxNeighborForMarriageWithNullPreference())!=-1){
+			this.pointingNode = j;
+		}
+	}
+	void abandonmentRule(){
+		MSNode temp = this.getNodeByID(this.pointingNode);
+		if(this.isMarried == this.PRmarried() && this.pointingNode!=-1 
+				&& temp.pointingNode!=this.ID 
+				&& (temp.isMarried || temp.ID <= this.ID)){
+			this.pointingNode = -1;
+		}
+	}
 	@Override
 	public void handleMessages(Inbox inbox) {
 		// TODO Auto-generated method stub
@@ -45,6 +132,7 @@ public class MSNode extends Node {
 		// TODO Auto-generated method stub
 		this.isMarried = false;
 		this.pointingNode = -1 ;
+		this.isEligibile = false;
 		
 	}
 
@@ -82,5 +170,12 @@ public class MSNode extends Node {
 			
 		}
 	}
+	
+	@NodePopupMethod(menuText="Change_Color")
+	public void myPopupMethod2(){
+		this.setColor(Color.RED);
+	}
+
+	
 
 }
